72ee26227577b9509d293f874b264ed9f37bf174
Robert Darwin <robert.darwin@outlook.com>
2017-05-16 22:28:26 +0800

    使 Android NDK 能识别并编译 Classes 目录下的代码文件

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -11,11 +11,44 @@ LOCAL_MODULE := MyGame_shared
 
 LOCAL_MODULE_FILENAME := libMyGame
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+#################### OUR CUSTOMIZATION ####################
 
+# 配置自己的源文件目录和源文件后缀名
+MY_FILES_PATH  :=  $(LOCAL_PATH)/hellocpp/ \
+		   $(LOCAL_PATH)/../../Classes
+
+MY_FILES_SUFFIX := %.cpp %.c
+
+# 递归遍历目录下的所有的文件
+rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+# 获取相应的源文件
+MY_ALL_FILES := $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*.*) )
+MY_ALL_FILES := $(MY_ALL_FILES:$(MY_CPP_PATH)/./%=$(MY_CPP_PATH)%)
+MY_SRC_LIST  := $(filter $(MY_FILES_SUFFIX),$(MY_ALL_FILES))
+MY_SRC_LIST  := $(MY_SRC_LIST:$(LOCAL_PATH)/%=%)
+
+# 去除字串的重复单词
+define uniq =
+  $(eval seen :=)
+  $(foreach _,$1,$(if $(filter $_,${seen}),,$(eval seen += $_)))
+  ${seen}
+endef
+
+# # 递归遍历获取所有目录
+# MY_ALL_DIRS := $(dir $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*/) ) )
+# MY_ALL_DIRS := $(call uniq,$(MY_ALL_DIRS))
+
+# 赋值给NDK编译系统
+LOCAL_SRC_FILES  := $(MY_SRC_LIST)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+#################### CUSTOMIZATION END ####################
+
+# LOCAL_SRC_FILES := hellocpp/main.cpp \
+#                    ../../Classes/AppDelegate.cpp \
+#                    ../../Classes/HelloWorldScene.cpp
+# LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
