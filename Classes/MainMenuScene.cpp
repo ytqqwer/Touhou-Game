579c154fa5818796bc8e6d17b8b168d6c75fb6fa
周海洋 <2776748886@qq.com>
2017-06-02 19:26:18 +0800

    MainMenuScene: 基本完成

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -3,8 +3,17 @@
 #endif
 
 #include "MainMenuScene.h"
+#include "GameData.h"
+#include "HomeScene.h"
 #include "NonGameplayScenesCache.h"
 #include "PlaceHolder.h"
+#include "RoundSelectScene.h"
+#include "SaveScene.h"
+#include "SettingsLayer.h"
+#include "ui/CocosGUI.h"
+
+using namespace ui;
+
 // #include "resources.h.dir/main_menu.h"
 #include <string>
 
@@ -15,6 +24,7 @@ const std::string MainMenuScene::TAG{ "MainMenuScene" };
 
 MainMenuScene::MainMenuScene()
 {
+    gamedata = GameData::getInstance();
     _visibleSize = _director->getVisibleSize();
 }
 
@@ -37,7 +47,7 @@ MainMenuScene::init()
     this->addChild(sceneTag);
 #endif
 
-    /*  3. PlaceHolder */
+    /*  3. PlaceHolder
 
     auto p = PlaceHolder::createCircle(100, "MainMenuScene");
     p->setPosition(_visibleSize / 2);
@@ -47,9 +57,87 @@ MainMenuScene::init()
 
     this->scheduleUpdate();
 
+    /*新游戏*/
+    auto NGButton = Button::create("", "", "");
+    NGButton->setTitleText("新游戏");
+    NGButton->setTitleColor(Color3B(194, 134, 11));
+    NGButton->setTitleFontSize(50);
+    NGButton->setAnchorPoint(Vec2(0, 0));
+    NGButton->setPosition(Vec2(_visibleSize.width * 0.8, _visibleSize.height * 0.57));
+    NGButton->addTouchEventListener([this](Ref* pSender, Widget::TouchEventType type) {
+        gamedata = GameData::getInstance();
+        auto lac = gamedata->newGame();
+
+        if (!lac) {
+            Director::getInstance()->pushScene(HomeScene::create());
+        }
+    });
+    addChild(NGButton);
+
+    /*载入游戏*/
+    auto LGButton = Button::create("", "", "");
+    LGButton->setTitleText("载入游戏");
+    LGButton->setTitleColor(Color3B(194, 134, 11));
+    LGButton->setTitleFontSize(50);
+    LGButton->setAnchorPoint(Vec2(0, 0));
+    LGButton->setPosition(Vec2(_visibleSize.width * 0.8, _visibleSize.height * 0.43));
+    LGButton->addTouchEventListener([](Ref* pSender, Widget::TouchEventType type) {
+        Director::getInstance()->pushScene(SaveScene::create());
+    }
+
+                                    );
+    addChild(LGButton);
+
+    /*设置*/
+    auto SGButton = Button::create("", "", "");
+    SGButton->setTitleText("设置");
+    SGButton->setTitleColor(Color3B(194, 134, 11));
+    SGButton->setTitleFontSize(30);
+    SGButton->setAnchorPoint(Vec2(0, 0));
+    SGButton->setPosition(Vec2(_visibleSize.width * 0.8, _visibleSize.height * 0.34));
+    SGButton->addTouchEventListener([this](Ref* pSender, Widget::TouchEventType type) {
+        if (type == Widget::TouchEventType::ENDED) {
+            auto lay = SettingsLayer::create("MainMenuScene");
+            this->addChild(lay, 5);
+        }
+    });
+
+    addChild(SGButton);
+    /*职员表*/
+    auto ZGButton = Button::create("", "", "");
+    ZGButton->setTitleText("职员表");
+    ZGButton->setTitleColor(Color3B(194, 134, 11));
+    ZGButton->setTitleFontSize(30);
+    ZGButton->setAnchorPoint(Vec2(0, 0));
+    ZGButton->setPosition(Vec2(_visibleSize.width * 0.8, _visibleSize.height * 0.27));
+    addChild(ZGButton);
+    /*退出游戏*/
+    auto ret = Button::create("", "", "");
+    ret->setTitleText("退出");
+    ret->setTitleColor(Color3B(184, 134, 11));
+    ret->setTitleFontSize(25);
+    ret->setAnchorPoint(Vec2(0, 0));
+    ret->setPosition(Vec2(_visibleSize.width * 0.8, _visibleSize.height * 0.2));
+
+    ret->addTouchEventListener(
+        [](Ref* pSender, Widget::TouchEventType type) { Director::getInstance()->end(); });
+    addChild(ret);
+
     return true;
 }
+void
+MainMenuScene::onEnter()
+{
+    Scene::onEnter();
 
+    auto loc = gamedata->getCurrentLocation();
+
+    /*背景*/
+    auto bg = Sprite::create(loc.backgroundPicture);
+    bg->setContentSize(_visibleSize);
+    bg->setPosition(_visibleSize / 2);
+    addChild(bg, -1);
+}
 void
 MainMenuScene::update(float dt)
 {
--- Classes/MainMenuScene.h
+++ Classes/MainMenuScene.h
@@ -6,8 +6,18 @@
 #define MAIN_MENU_SCENE_H
 
 #include "AppMacros.h"
+#include "GameData.h"
+#include "GameData/Character.h"
+#include "GameData/Conversation.h"
+#include "GameData/Item.h"
+#include "GameData/Location.h"
+#include "GameData/Round.h"
+#include "GameData/SpellCard.h"
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
 #include <string>
+#include <string>
+#include <vector>
 
 USING_NS_CC;
 
@@ -17,6 +27,7 @@ public:
     APP_SCENE_CREATE_FUNC(MainMenuScene, TAG);
 
     virtual bool init();
+    virtual void onEnter();
     virtual void update(float dt);
 
 private:
@@ -27,6 +38,7 @@ private:
 
     // intorspection
     Size _visibleSize;
+    GameData* gamedata;
 };
 
 #endif // MAIN_MENU_SCENE_H
